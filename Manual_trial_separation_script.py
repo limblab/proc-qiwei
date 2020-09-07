9a986d68901b6f7342f453009566c5d4cea95fab
Qiwei Dong <dongqiweialexander@gmail.com>
2020-09-07 16:21:32 -0500

    finished fixing manual-trial_separation_script

--- Manual_trial_separation_script.py
+++ Manual_trial_separation_script.py
@@ -3,24 +3,43 @@
 Created on Thu Aug 20 15:43:04 2020
 
 @author: dongq
+
+Use a self-created ground truth file as a reference to separate the trials
+manually to a data file with the same data structure, but containing only
+the frames during experiment.
+
+The ground-truth file should be in .txt
+And the structure inside should be something like:
+
+START_OF_EXPERIMENT_PHASE_MINUTE [space] START_OF_EXPERIMENT_PHASE_SECOND [space] END_OF_EXPERIMENT_PHASE_MINUTE [space] END_OF_EXPERIMENT_PHASE_SECOND [space]
+
+For example:
+0 50 3 45 
+Means: From 0 minutes,50 seconds to 3 minutes,45 seconds in the video, the monkey
+is actually reaching to the targets (doing the experiment), instead of idling
+around.
+
+(***Remember there's a space after the 4th number of each row, result of being lazy on the code)
 """
 
 #%% Import Packages
 import pandas as pd 
 import numpy as np
-import os
-import math
-import matplotlib.pyplot as plt
-from scipy.signal import find_peaks
+#import os
+#import math
+#import matplotlib.pyplot as plt
+#from scipy.signal import find_peaks
 #from peakutils.plot import plot as pplot
 #import peakutils
-from scipy import signal
-from scipy.interpolate import interp1d
+#from scipy import signal
+#from scipy.interpolate import interp1d
 #from moviepy.editor import *
-from scipy import stats
-import copy
+#from scipy import stats
+#import copy
 #%% Read in dataset for segmentation
-df = pd.read_csv (r'C:\Users\dongq\DeepLabCut\Han-Qiwei-2020-08-04-RandomTarget\reconstructed-3d-data\output_3d_data.csv')
+dataset_path = r'C:\Users\dongq\DeepLabCut\Han-Qiwei-2020-08-04-RandomTarget\reconstructed-3d-data'
+
+df = pd.read_csv (dataset_path + '\output_3d_data.csv')
 
 #df = pd.read_csv (r'C:\Users\dongq\DeepLabCut\Han-Qiwei-2020-08-04-FreeReaching\reconstructed-3d-data\output_3d_data.csv')
 
@@ -64,11 +83,21 @@ for i in range(len(f_frame_list)):
     #print(i)
     ground_truth_segment[f_frame_list[i]] = 1
     
-#%% Append a row to the new DataFrame if ground_truth_segment[i] == 1
+#%% Define a trial segmentation function
+def experiment_trial_segment(df,ground_truth_list):
+    df2 = pd.DataFrame(np.zeros((0,df.shape[1])),columns = df.columns)
+    for i in range(len(ground_truth_list)):
+        df2.loc[i] = df.iloc[ground_truth_list[i]]
+    #print(df2)
+    return df2
+
+#%% Separate the dataset
+df_exp_only = experiment_trial_segment(df, f_frame_list)
     
 #%% Save the DataFrame as a .csv file in the same folder as the original dataset
+df_exp_only.to_csv(dataset_path + '\output_3d_dataset_exp_only.csv')
 
-#%%
+#%% (DEPRICATED)
 """
 def count_non_confident_points_3D(df):
     #df_head = df.head()
@@ -86,9 +115,6 @@ def count_non_confident_points_3D(df):
         df_dropouts.append(dropout_nums)
     return df_dropouts
 """
-#%%
 """
 non_confident_3D_points = count_non_confident_points_3D(df)
 """
-
-#%%
